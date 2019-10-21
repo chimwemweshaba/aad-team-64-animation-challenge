97276c75e50eabf32618892a2c53a079336ae983
Chimwemwe Shaba <chimwemweshaba@gmail.com>
2019-10-21 11:05:33 +0200

    feat: home screen

---
+++ app/src/main/java/com/aadteam64/groupchallenge/FirstActivity.kt
@@ -0,0 +1,20 @@
+package com.aadteam64.groupchallenge
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_first.*
+
+class FirstActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first)
+
+        buttonStart.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
